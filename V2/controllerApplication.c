76892b38ab3247918568b4dd9c28b5cc4f135bb0
Lannuzel <lannuzel.timothe@live.fr>
2021-02-10 17:19:45 +0100

    0.3.3

--- V2/controllerApplication.c
+++ V2/controllerApplication.c
@@ -2,6 +2,9 @@
 #include <stdio.h>
 #include <po_hi_time.h>
 
+int gps =0;
+int ins =0;
+
 void controller_spg (void)
 {
   printf ("[%d] Controller\n", milliseconds_since_epoch());
@@ -13,3 +16,32 @@ void dataProc_spg (void)
   printf ("[%d] DataProcessor\n", milliseconds_since_epoch());
   fflush (stdout);
 }
+
+void write_gps (int* value)
+{
+  int v = *value;
+  v++;
+  *value = v;
+  printf ("Value Write : %d\n", *value);
+}
+
+void read_gps (int* value)
+{
+  printf ("Value: %d\n", *value);
+  gps=*value;
+}
+
+void write_ins (int* value)
+{
+  int v = *value;
+  v++;
+  *value = v;
+  printf ("Value Write : %d\n", *value);
+}
+
+void read_ins (int* value)
+{
+  printf ("Value: %d\n", *value);
+  gps=*value;
+}
+
